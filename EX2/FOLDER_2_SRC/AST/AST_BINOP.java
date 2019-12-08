4a7b1803e2411086a82425ed5a52e3c2288e2b72
Offek Gil <43298246+offekg@users.noreply.github.com>
2019-12-08 21:46:59 +0200

    fixed BINOP PrintMe + const. syso

--- EX2/FOLDER_2_SRC/AST/AST_BINOP.java
+++ EX2/FOLDER_2_SRC/AST/AST_BINOP.java
@@ -2,11 +2,26 @@ package AST;
 
 public class AST_BINOP extends AST_Node {
 	public int OP;
+	public String sOP="";
 	public AST_BINOP(int op){
 		this.OP = op;
 		/* SET A UNIQUE SERIAL NUMBER */
 		/******************************/
 		SerialNumber = AST_Node_Serial_Number.getFresh();
+		/*********************************/
+		/* CONVERT OP to a printable sOP */
+		/*********************************/
+		if (this.OP == 0) {this.sOP = "+";}
+		if (this.OP == 1) {this.sOP = "-";}
+		if (this.OP == 2) {this.sOP = "*";}
+		if (this.OP == 3) {this.sOP = "/";}
+		if (this.OP == 4) {this.sOP = "<";}
+		if (this.OP == 5) {this.sOP = ">";}
+		if (this.OP == 6) {this.sOP = "=";}
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== BINOP ->"+this.sOP+"\n");
 	}
 
 	/******************************************************/
@@ -14,8 +29,9 @@ public class AST_BINOP extends AST_Node {
 	/******************************************************/
 	public void PrintMe()
 	{
+		
 		/**************************************/
-		/* AST NODE TYPE = AST STATEMENT LIST */
+		/* AST NODE TYPE = AST BINOP */
 		/**************************************/
 		System.out.print("AST NODE BINOP\n");
 
@@ -24,6 +40,6 @@ public class AST_BINOP extends AST_Node {
 		/**********************************/
 		AST_GRAPHVIZ.getInstance().logNode(
 			SerialNumber,
-			"BINOP\n");
+			String.format("BINOP(%s)\n",this.sOP)");
 	}
 }
