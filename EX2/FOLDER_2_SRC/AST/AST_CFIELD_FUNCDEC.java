e4f20280f5fe6f69212530ef34437c29c6633e63
Offek Gil <43298246+offekg@users.noreply.github.com>
2019-12-09 01:40:28 +0200

    updated  CFIELD_FUNCDEC const. SYSO and print_me

--- EX2/FOLDER_2_SRC/AST/AST_CFIELD_FUNCDEC.java
+++ EX2/FOLDER_2_SRC/AST/AST_CFIELD_FUNCDEC.java
@@ -2,23 +2,29 @@ package AST;
 
 public abstract class AST_CFIELD_FUNCDEC extends AST_CFIELD {
 	public AST_DEC_FUNCDEC funcdec;
+	
 	public AST_CFIELD_FUNCDEC(AST_DEC_FUNCDEC funcdec) {
 		this.funcdec = funcdec;
 		SerialNumber = AST_Node_Serial_Number.getFresh();
+		
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== CFIELD -> FUNCDEC"\n");
 	}
 
 	/******************************************************/
-	/* The printing message for a statement list AST node */
+	/* The printing message for a CFIELD_FUNCDEC AST node */
 	/******************************************************/
 	public void PrintMe()
 	{
 		/**************************************/
-		/* AST NODE TYPE = AST STATEMENT LIST */
+		/* AST NODE TYPE = AST CFIELD_FUNCDEC */
 		/**************************************/
 		System.out.print("AST NODE CFIELD FUNC DEC\n");
 
 		/*************************************/
-		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/* RECURSIVELY PRINT SONS OF NODE... */
 		/*************************************/
 		if (funcdec != null) funcdec.PrintMe();
 
