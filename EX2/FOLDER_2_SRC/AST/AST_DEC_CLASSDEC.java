99a747e0f03c817bbe9e0788db824b94392b8e91
Offek Gil <43298246+offekg@users.noreply.github.com>
2019-12-09 02:21:13 +0200

    updated classdec const. print + print_me

--- EX2/FOLDER_2_SRC/AST/AST_DEC_CLASSDEC.java
+++ EX2/FOLDER_2_SRC/AST/AST_DEC_CLASSDEC.java
@@ -4,11 +4,17 @@ public class AST_DEC_CLASSDEC extends AST_DEC {
 	public String name1;
 	public String name2;
 	public AST_CFIELD_LIST cFieldList;
+	
 	public AST_DEC_CLASSDEC(String name1, String name2, AST_CFIELD_LIST cFieldList) {
 		this.name1 = name1;
 		this.name2 = name2;
 		this.cFieldList = cFieldList;
 		SerialNumber = AST_Node_Serial_Number.getFresh();
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== Dec (CLASS) -> CLASS DEC"\n");
+	}
 	}
 
 	/******************************************************/
@@ -17,9 +23,12 @@ public class AST_DEC_CLASSDEC extends AST_DEC {
 	public void PrintMe()
 	{
 		/**************************************/
-		/* AST NODE TYPE = AST STATEMENT LIST */
+		/* AST NODE TYPE = AST DEC  CLASS DEC */
 		/**************************************/
-		System.out.print("AST NODE CLASS DEC\n");
+		if (name2 == null) 
+		    System.out.print("AST NODE CLASS DEC:\n Class %s\n",this.name1);
+		else  
+		    System.out.print("AST NODE CLASS DEC:\n Class %s Extends %s\n",this.name1,this.name2);
 
 		/*************************************/
 		/* RECURSIVELY PRINT HEAD + TAIL ... */
@@ -29,9 +38,17 @@ public class AST_DEC_CLASSDEC extends AST_DEC {
 		/**********************************/
 		/* PRINT to AST GRAPHVIZ DOT file */
 		/**********************************/
-		AST_GRAPHVIZ.getInstance().logNode(
+		if (name2 == null) {
+		    AST_GRAPHVIZ.getInstance().logNode(
 			SerialNumber,
-			"CLASS\nDEC\n");
+			String.format("CLASS DEC\n Class %s\n",this.name1));
+		}
+		else  {
+		    AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("CLASS DEC\n Class %s Extends %s\n",this.name1,this.name2));
+		}
+		
 		
 		/****************************************/
 		/* PRINT Edges to AST GRAPHVIZ DOT file */
