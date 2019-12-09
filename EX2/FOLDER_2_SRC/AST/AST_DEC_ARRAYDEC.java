418c2c1df6e63dd6d1bb8910c26212cee82713db
Offek Gil <43298246+offekg@users.noreply.github.com>
2019-12-09 02:10:44 +0200

    updated dec_Arraydec const. print and print_me

--- EX2/FOLDER_2_SRC/AST/AST_DEC_ARRAYDEC.java
+++ EX2/FOLDER_2_SRC/AST/AST_DEC_ARRAYDEC.java
@@ -3,10 +3,15 @@ package AST;
 public class AST_DEC_ARRAYDEC extends AST_DEC {
 	public String name1;
 	public String name2;
+	
 	public AST_DEC_ARRAYDEC(String name1, String name2) {
 		this.name1 = name1;
 		this.name2 = name2;
 		SerialNumber = AST_Node_Serial_Number.getFresh();
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== Dec (Array) -> ARRAY DEC"\n");
 	}
 
 	/******************************************************/
@@ -15,15 +20,16 @@ public class AST_DEC_ARRAYDEC extends AST_DEC {
 	public void PrintMe()
 	{
 		/**************************************/
-		/* AST NODE TYPE = AST STATEMENT LIST */
+		/* AST NODE TYPE = AST ARRAY DEC      */
 		/**************************************/
-		System.out.print("AST NODE ARRAY DEC\n");
+		System.out.print("AST NODE ARRAY DEC: ARRAY"+ this.name1 + " = " = this.name2 + "[]\n");
 
 		/**********************************/
 		/* PRINT to AST GRAPHVIZ DOT file */
 		/**********************************/
 		AST_GRAPHVIZ.getInstance().logNode(
 			SerialNumber,
-			"ARRAY\nDEC\n");
+			String.format("ARRAY DEC\n arr %s = %s[]\n", this.name1, this.name2));
 	}
 }
+ 
