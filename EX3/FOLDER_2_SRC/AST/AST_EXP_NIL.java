6cfa02659c9397fb21c6cf7cff076a0f439a229e
IdoLerer <idolerer222@gmail.com>
2019-12-24 18:42:01 +0200

    Compilation errors

--- EX3/FOLDER_2_SRC/AST/AST_EXP_NIL.java
+++ EX3/FOLDER_2_SRC/AST/AST_EXP_NIL.java
@@ -25,10 +25,6 @@ public class AST_EXP_NIL extends AST_EXP {
 			SerialNumber,
 			"EXP\nNIL\n");
 	}
-			public TYPE SemantMe()
-	{
-		return TYPE_INT.getInstance();
-	}
 
 	public TYPE SemantMe()
 	{
