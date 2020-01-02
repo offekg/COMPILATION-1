8e86954ca8564a6e32972c7d0dbf73696d795c37
Offek Gil <Offekg@gmail.com>
2020-01-02 15:20:09 +0200

    fixed params can't have existing type name in AST_FUNC_INPUT_VARS

--- EX3/FOLDER_2_SRC/AST/AST_FUNC_INPUT_VARS.java
+++ EX3/FOLDER_2_SRC/AST/AST_FUNC_INPUT_VARS.java
@@ -24,10 +24,15 @@ public class AST_FUNC_INPUT_VARS extends AST_Node {
 			OutputFileWriter.writeError(this.lineNumber, String.format("func_var type not legal: %s", paramType));
 		}
 		
-		// check if name already exists
+		// check if name already exists in other params 
 		if (SYMBOL_TABLE.getInstance().isInScope(paramName)) {
 			OutputFileWriter.writeError(this.lineNumber,
 					String.format("func_var name already exists in func %s", paramName));
+		}
+		//check if name exists as a type
+		TYPE temp = SYMBOL_TABLE.getInstance().find(paramName);
+		if (temp != null && temp.name.equals(paramName) ) {
+			OutputFileWriter.writeError(this.lineNumber, String.format("param %s already exists as a type\n", paramName));
 		}
 
 		SYMBOL_TABLE.getInstance().enter(paramName, typeOfParam);
