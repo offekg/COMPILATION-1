b1de79051cdfa6242bdcbd640ee1bbba92e0e553
Offek Gil <Offekg@gmail.com>
2020-02-27 01:07:18 +0200

    started array access

--- EX4/FOLDER_2_SRC/IR/IRcommand_Array_Access.java
+++ EX4/FOLDER_2_SRC/IR/IRcommand_Array_Access.java
@@ -31,6 +31,14 @@ public class IRcommand_Array_Access extends IRcommand
 	/***************/
 	public void MIPSme()
 	{
+		//need to check index is valid
+		TEMP arrayPointer = TEMP_FACTORY.getInstance().getFreshTEMP(); //how to use s registers?
+		sir_MIPS_a_lot.getInstance().move(arrayPointer,subscriptTemp);
+		sir_MIPS_a_lot.getInstance().addi(arrayPointer, arrayPointer, 1);
+		TEMP multiplier = TEMP_FACTORY.getInstance().getFreshTEMP();
+		sir_MIPS_a_lot.getInstance().li(multiplier, 4);
+		sir_MIPS_a_lot.getInstance().mul(arrayPointer, arrayPointer, multiplier);
+		
 	}
 
 	@Override
