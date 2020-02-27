b6ee8e951a6dfd17d2edbf180868cb019105d5db
EizTimor <timor.eizenman@gmail.com>
2020-02-27 21:15:24 +0200

    added integer overflow handler

--- EX4/FOLDER_2_SRC/IR/IRcommand.java
+++ EX4/FOLDER_2_SRC/IR/IRcommand.java
@@ -3,6 +3,7 @@
 /***********/
 package IR;
 
+import MIPS.*;
 import UTILS.Context;
 
 /*******************/
@@ -13,26 +14,51 @@ import UTILS.Context;
 /* PROJECT IMPORTS */
 /*******************/
 
-public abstract class IRcommand
-{
+public abstract class IRcommand {
 	/*****************/
 	/* Label Factory */
 	/*****************/
-	protected static int label_counter=-1;
-	public    static String getFreshLabel(String msg)
-	{
+	protected static int label_counter = -1;
+
+	public static String getFreshLabel(String msg) {
 		label_counter++;
-		while (Context.classNames.contains(String.format("Label_%d",label_counter))) {
+		while (Context.classNames.contains(String.format("Label_%d", label_counter))) {
 			label_counter++;
 		}
-		return String.format("Label_%d_%s",label_counter,msg);
+		return String.format("Label_%d_%s", label_counter, msg);
 	}
 
 	/***************/
 	/* MIPS me !!! */
 	/***************/
 	public abstract void MIPSme();
-	
+
 	/* print function for debugging */
 	public abstract void printMe();
+
+	public void integerOverflowHandler(TEMP t) {
+		TEMP minValue = TEMP_FACTORY.getInstance().getFreshTEMP();
+		TEMP maxValue = TEMP_FACTORY.getInstance().getFreshTEMP();
+
+		sir_MIPS_a_lot.getInstance().li(minValue, -32768);
+		sir_MIPS_a_lot.getInstance().li(maxValue, 32767);
+
+		String integerValidLabel = getFreshLabel("valid");
+		String minOverflowLabel = getFreshLabel("min_overflow");
+		String maxOverflowLabel = getFreshLabel("max_overflow");
+
+		sir_MIPS_a_lot.getInstance().bge(minValue, t, minOverflowLabel);
+		sir_MIPS_a_lot.getInstance().blt(maxValue, t, maxOverflowLabel);
+		sir_MIPS_a_lot.getInstance().jump(integerValidLabel);
+
+		sir_MIPS_a_lot.getInstance().label(minOverflowLabel);
+		sir_MIPS_a_lot.getInstance().li(t, -32768);
+		sir_MIPS_a_lot.getInstance().jump(integerValidLabel);
+
+		sir_MIPS_a_lot.getInstance().label(maxOverflowLabel);
+		sir_MIPS_a_lot.getInstance().li(t, 32767);
+		sir_MIPS_a_lot.getInstance().jump(integerValidLabel);
+
+		sir_MIPS_a_lot.getInstance().label(integerValidLabel);
+	}
 }
--- EX4/FOLDER_2_SRC/IR/IRcommand_Binop_Subtract_Integers.java
+++ EX4/FOLDER_2_SRC/IR/IRcommand_Binop_Subtract_Integers.java
@@ -13,29 +13,27 @@ package IR;
 import TEMP.*;
 import MIPS.*;
 
-public class IRcommand_Binop_Subtract_Integers extends IRcommand
-{
+public class IRcommand_Binop_Subtract_Integers extends IRcommand {
 	public TEMP t1;
 	public TEMP t2;
 	public TEMP dst;
-	
-	public IRcommand_Binop_Subtract_Integers(TEMP dst,TEMP t1,TEMP t2)
-	{
+
+	public IRcommand_Binop_Subtract_Integers(TEMP dst, TEMP t1, TEMP t2) {
 		this.dst = dst;
 		this.t1 = t1;
 		this.t2 = t2;
 	}
+
 	/***************/
 	/* MIPS me !!! */
 	/***************/
-	public void MIPSme()
-	{
-		sir_MIPS_a_lot.getInstance().sub(dst,t1,t2);
+	public void MIPSme() {
+		sir_MIPS_a_lot.getInstance().sub(dst, t1, t2);
+		integerOverflowHandler(dst);
 	}
-	
+
 	@Override
 	public void printMe() {
-		System.out.println(dst.getSymbol() + " = sub " + t1.getSymbol()
-							+ ", " + t2.getSymbol());
+		System.out.println(dst.getSymbol() + " = sub " + t1.getSymbol() + ", " + t2.getSymbol());
 	}
 }
