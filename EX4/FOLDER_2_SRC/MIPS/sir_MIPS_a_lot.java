543591f34e5b67271fa7a5c721c8c20c81b09f15
orinL <49723054+orinL@users.noreply.github.com>
2020-03-02 10:14:47 +0200

    empty label for empty VT

--- EX4/FOLDER_2_SRC/MIPS/sir_MIPS_a_lot.java
+++ EX4/FOLDER_2_SRC/MIPS/sir_MIPS_a_lot.java
@@ -414,6 +414,9 @@ public class sir_MIPS_a_lot {
 			if(!methods.equals("\n")){
 				add_to_global_data_list("VT_" + class_name, ".word", methods);
 			}
+			else{
+				add_to_global_data_list("VT_" + class_name, "", "\n");
+			}
 		}
 	}
 
