11ae36c51a6f54eaa01b2ae93f014b33b65c5f2f
EizTimor <timor.eizenman@gmail.com>
2020-03-05 23:15:40 +0200

    added new IR

---
+++ EX4/FOLDER_2_SRC/IR/IRcommand_StoreOnStack.java
@@ -0,0 +1,26 @@
+package IR;
+
+import MIPS.sir_MIPS_a_lot;
+import TEMP.*;
+
+public class IRcommand_StoreOnStack extends IRcommand{
+	public TEMP t;
+	public int offset;
+	
+	public IRcommand_StoreOnStack(TEMP t, int offset)
+	{
+		this.t = t;
+		this.offset = offset;
+	}
+
+	
+	public void MIPSme() 
+	{
+		sir_MIPS_a_lot.getInstance().storeROnStack(t, offset);
+	}
+	
+	@Override
+	public void printMe() {
+		System.out.println("store on stack " + t.getSymbol() + " offset " + offest);
+	}
+}
